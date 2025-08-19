c14836a79dee79f445b5e70ac001e25a8868acd7
Yu Ling Chen <yo0.guitar.it@gmail.com>
2025-08-20 02:06:42 +0800

    feat: 移除不再使用的 BlogLayout 和相關的文章處理邏輯，更新依賴以支持 MDX

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,21 +1,9 @@
-import Link from 'next/link';
-import { getSortedPostsData, PostData } from '@/app/lib/posts';
-
-export default function BlogPage() {
-  const allPostsData = getSortedPostsData();
-
+function BlogPage() {
   return (
     <section>
       <h2>Blog</h2>
-      <ul>
-        {allPostsData.map(({ id, date, title }: PostData) => (
-          <li key={id}>
-            <Link href={`/blog/${id}`}>{title}</Link>
-            <br />
-            <small>{date}</small>
-          </li>
-        ))}
-      </ul>
     </section>
   );
 }
+
+export default BlogPage;
